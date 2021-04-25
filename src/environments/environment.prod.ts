923a7316b506951f51cd87ac898d72122c27e71b
Rafael <rafaelkachlon@gmail.com>
2021-04-26 00:04:51 +0300

    added hover to favorite. change environment prod to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiKey: '3MkiBAOiZjXukWY1sNyLfiwtWUTO5XMJ',
-  baseUrl: 'http://dataservice.accuweather.com/',
+  baseUrl: 'https://dataservice.accuweather.com/',
   autoComplete: 'locations/v1/cities/autocomplete',
   currentCondition: 'currentconditions/v1/',
   fiveDaysForecast: 'forecasts/v1/daily/5day/',
